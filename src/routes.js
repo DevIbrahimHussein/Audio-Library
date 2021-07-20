9d5ee27e73b434fc3c207cd6391ab47f71ad1c0c
Ibrahim Hussein <ibrahimhussein19.98@gmail.com>
2021-07-20 23:47:50 +0300

    end points added

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,74 @@
 const express = require('express')
+const { listAlbums, addAlbum, updateAlbum, deleteAlbum } = require('./controllers/album.controller')
+const { addCategory, listCategories, updateCategory, deleteCategory } = require('./controllers/category.controller')
+const { addTrack, listTracks, updateTrack, deleteTrack } = require('./controllers/track.controller')
 const router = express.Router()
 
+const { dataHandler } = require('./middleware/route-level.middleware')
 // ========================================================================================================================
 
-router.post('/category')
+router.post('/category',
+    addCategory,
+    dataHandler
+)
 
-router.get('/categories')
+router.get('/categories',
+    listCategories,
+    dataHandler
+)
 
-router.put('/category/:categoryId')
+router.put('/category/:categoryId',
+    updateCategory,
+    dataHandler
+)
 
-router.delete('/category/:categoryId')
+router.delete('/category/:categoryId',
+    deleteCategory,
+    dataHandler
+)
 
 // ========================================================================================================================
 
-router.post('/album')
+router.post('/album',
+    addAlbum,
+    dataHandler
+)
 
-router.get('/albums')
+router.get('/albums',
+    listAlbums,
+    dataHandler
+)
 
-router.put('/album/:categoryId')
+router.put('/album/:categoryId',
+    updateAlbum,
+    dataHandler
+)
 
-router.delete('/album/:categoryId')
+router.delete('/album/:categoryId',
+    deleteAlbum,
+    dataHandler
+)
 
 // ========================================================================================================================
 
-router.post('/song')
+router.post('/song',
+    addTrack,
+    dataHandler
+)
 
-router.get('/songs')
+router.get('/songs',
+    listTracks,
+    dataHandler
+)
 
-router.put('/song/:songId')
+router.put('/song/:songId',
+    updateTrack,
+    dataHandler
+)
 
-router.delete('/song:SongId')
-
+router.delete('/song:SongId',
+    deleteTrack,
+    dataHandler
+)
 
 module.exports = router 
