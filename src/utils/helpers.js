d151bf7142d15c392d6d66c926de92229253e750
Ibrahim Hussein <ibrahimhussein19.98@gmail.com>
2021-07-27 04:24:26 +0300

    Nodemailer added without testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,30 @@
 const mongoose = require("mongoose")
+const nodemailer = require('nodemailer')
+const user = 'ibrahimhussein19.98@gmail.com'
+const pass = 'gibshniirjtiqfwx'
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    auth: {
+        user: user,
+        pass: pass
+    }
+})
 
 module.exports = {
 
     convertToObject(strId){
         return mongoose.Types.ObjectId(strId)
+    },
+
+    sendEmail(){
+        const mailOptions = {
+            from: from,
+            to: 'support@bobshop.com',  
+            subject: subject,
+            text: message,
+        }
+        transporter.sendMail(mailOptions)
     }
 
 }
